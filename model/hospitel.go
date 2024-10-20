b27562166c484202fac2daa8dc50b24591fc1185
salmanfaris22 <salman567633800@gmail.com>
2024-10-20 18:28:33 +0530

    Golang

--- model/hospitel.go
+++ model/hospitel.go
@@ -3,22 +3,25 @@ package model
 import "time"
 
 // Patient Model
-type Patient struct {
-	ID    uint   `gorm:"primaryKey:autoIncrement"`
-	Name  string `gorm:"size:24"`
-	Phone string `gorm:"size:24"`
-	Age   int    `gorm:"size:2"`
-	Place string `gorm:"size:24"`
-}
+////type Patient struct {
+//	ID    uint   `gorm:"primaryKey:autoIncrement"`
+//	Name  string `gorm:"size:24"`
+//	Phone string `gorm:"size:24"`
+//	Age   int    `gorm:"size:2"`
+//	Place string `gorm:"size:24"`
+////}
 
 // Appointment Model
 type Appointment struct {
-	TokenID   uint      `gorm:"primaryKey:autoIncrement"`
-	Date      time.Time `gorm:"type:date"`
-	PatientID uint      `gorm:"not null"`
-	DoctorID  uint      `gorm:"not null"`
-	Patient   Patient   `gorm:"foreignKey:PatientID;references:ID"`
-	Doctor    Doctor    `gorm:"foreignKey:DoctorID;references:DoctID"`
+	TokenID     uint      `gorm:"primaryKey:autoIncrement"`
+	PatientName uint      `gorm:"size:24"`
+	Age         int       `gorm:"size:2"`
+	Place       string    `gorm:"size:24"`
+	Date        time.Time `gorm:"type:date"`
+	UserID      uint      `gorm:"not null"`
+	DoctorID    uint      `gorm:"not null"`
+	User        User      `gorm:"foreignKey:UserID;references:ID"`
+	Doctor      Doctor    `gorm:"foreignKey:DoctorID;references:DoctID"`
 }
 
 // Doctor Model
